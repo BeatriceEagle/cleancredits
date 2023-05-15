ca338247a267366ed2c62b74dc663d77befee710
sandalwoodbox <boxofsandalwood@gmail.com>
2023-05-14 23:23:25 -0700

    Fixed input mask handling by mask command

--- cleancredits/cli_test.py
+++ cleancredits/cli_test.py
@@ -8,15 +8,16 @@ from .helpers_test import TESTDATA_PATH
 
 
 @pytest.mark.parametrize(
-    "hue_min,hue_max,sat_min,sat_max,val_min,val_max,grow,bbox_x1,bbox_x2,bbox_y1,bbox_y2",
+    "hue_min,hue_max,sat_min,sat_max,val_min,val_max,grow,bbox_x1,bbox_x2,bbox_y1,bbox_y2,input_mask",
     [
-        (0, 179, 0, 255, 0, 255, 0, 0, None, 0, None),
-        (10, 20, 30, 50, 40, 60, 1, 100, 500, 150, 700),
-        (-5, 200, -10, 290, -50, 365, 500, 2000, 2000, 9001, 9001),
-        (200, -5, 290, -10, 365, -50, -1, -20, -21, -200, -2000),
-        (30, 60, 20, 200, 0, 240, 3, 20, 900, 35, 500),
-        (0, 179, 0, 45, 221, 255, 2, 302, 762, 593, 678),
-        (21, 87, 11, 143, 49, 109, 0, 588, 985, 455, 709),
+        (0, 179, 0, 255, 0, 255, 0, 0, None, 0, None, None),
+        (10, 20, 30, 50, 40, 60, 1, 100, 500, 150, 700, None),
+        (-5, 200, -10, 290, -50, 365, 500, 2000, 2000, 9001, 9001, None),
+        (200, -5, 290, -10, 365, -50, -1, -20, -21, -200, -2000, None),
+        (30, 60, 20, 200, 0, 240, 3, 20, 900, 35, 500, None),
+        (0, 179, 0, 45, 221, 255, 2, 302, 762, 593, 678, None),
+        (21, 87, 11, 143, 49, 109, 0, 588, 985, 455, 709, None),
+        (-5, 200, -10, 290, -50, 365, 500, 2000, 2000, 9001, 9001, "horses-720p-mask"),
     ],
 )
 def test_mask_no_gui(
@@ -31,39 +32,51 @@ def test_mask_no_gui(
     bbox_x2,
     bbox_y1,
     bbox_y2,
+    input_mask,
     tmp_path,
 ):
     # Run through a few basic scenarios
+    args = [
+        f"{TESTDATA_PATH / 'horses-720p.mp4'}",
+        f"--output={tmp_path / 'mask.png'}",
+        "--no-gui",
+        "--hue-min",
+        hue_min,
+        "--hue-max",
+        hue_max,
+        "--sat-min",
+        sat_min,
+        "--sat-max",
+        sat_max,
+        "--val-min",
+        val_min,
+        "--val-max",
+        val_max,
+        "--grow",
+        grow,
+        "--bbox-x1",
+        bbox_x1,
+        "--bbox-y1",
+        bbox_y1,
+    ]
+
+    if bbox_x2 is not None:
+        args += [
+            "--bbox-x2",
+            bbox_x2,
+        ]
+    if bbox_y2 is not None:
+        args += [
+            "--bbox-y2",
+            bbox_y2,
+        ]
+    if input_mask is not None:
+        args += ["-i", f"{TESTDATA_PATH / input_mask}.png"]
+
     runner = CliRunner()
     result = runner.invoke(
         mask,
-        [
-            f"{TESTDATA_PATH / 'horses-720p.mp4'}",
-            f"--output={tmp_path / 'mask.png'}",
-            "--no-gui",
-            "--hue-min",
-            hue_min,
-            "--hue-max",
-            hue_max,
-            "--sat-min",
-            sat_min,
-            "--sat-max",
-            sat_max,
-            "--val-min",
-            val_min,
-            "--val-max",
-            val_max,
-            "--grow",
-            grow,
-            "--bbox-x1",
-            bbox_x1,
-            "--bbox-x2",
-            bbox_x2,
-            "--bbox-y1",
-            bbox_y1,
-            "--bbox-y2",
-            bbox_y2,
-        ],
+        args,
         standalone_mode=False,
     )
     assert result.exception is None, result.output
@@ -72,7 +85,7 @@ def test_mask_no_gui(
     expected_mask_path = str(
         TESTDATA_PATH
         / "horses-720p-masks"
-        / f"mask-{hue_min}-{hue_max}-{sat_min}-{sat_max}-{val_min}-{val_max}-{grow}-{bbox_x1}-{bbox_x2}-{bbox_y1}-{bbox_y2}.png"
+        / f"mask-{hue_min}-{hue_max}-{sat_min}-{sat_max}-{val_min}-{val_max}-{grow}-{bbox_x1}-{bbox_x2}-{bbox_y1}-{bbox_y2}-{input_mask}.png"
     )
     expected_mask = cv2.imread(expected_mask_path)
     expected_mask = cv2.cvtColor(expected_mask, cv2.COLOR_BGR2GRAY)
